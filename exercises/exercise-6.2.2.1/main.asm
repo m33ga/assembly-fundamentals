88072af848fed7405c18146f9ee546e53dd85d40
Mihai Gurduza <mihai.gurduza@isa.utm.md>
2025-04-26 23:13:00 +0300

    add exercise 6.2.2.1

---
+++ exercises/exercise-6.2.2.1/main.asm
@@ -0,0 +1,29 @@
+; exercise 6.2.2.1
+
+.386
+.model flat,stdcall
+.stack 4096
+ExitProcess PROTO, dwExitCode:DWORD
+
+.code
+main PROC
+    ; Assign initial values
+    mov eax, 10    ; A = 10
+    mov ebx, 5     ; B = 5
+    mov ecx, 3     ; C = 3
+    mov edx, 2     ; D = 2
+
+    ; (A + B)
+    add eax, ebx
+
+    ; (C + D)
+    add ecx, edx
+
+    ; (A + B) - (C + D) 
+    sub eax, ecx
+
+    ; EAX = (A + B) - (C + D)
+
+    INVOKE ExitProcess, eax
+main ENDP
+END main
