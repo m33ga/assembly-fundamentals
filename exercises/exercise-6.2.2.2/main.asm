9c5d7577e91ee3cc786530691f8773d0ac1cf6e9
Mihai Gurduza <mihai.gurduza@isa.utm.md>
2025-04-27 13:44:39 +0300

    add exercise 6.2.2.2

---
+++ exercises/exercise-6.2.2.2/main.asm
@@ -0,0 +1,27 @@
+; exercise 6.2.2.2
+
+; just changing DWORD to QWORD is no bueno
+; more problems:
+;   .386 only supports 32-bit registers (EAX, EBX, etc).
+;   stdcall is for 32 bit functions — 64-bit uses a different calling 
+;   also need to change solution to x64 in configuration manager
+
+ExitProcess proto
+
+.data
+firstval  QWORD 2000200020002000h
+secondval QWORD 1111111111111111h
+thirdval  QWORD 2222222222222222h
+sum       QWORD 0
+
+.code
+main proc
+    mov rax, firstval
+    add rax, secondval
+    add rax, thirdval
+    mov sum, rax
+
+    call ExitProcess
+main endp
+
+end
